26b86a843b8bf27c5f4ede5419ee11e4e564fde0
Savy <savyportz@gmail.com>
2019-05-13 17:48:57 -0500

    Added target var
    not sure if it will be needed, just in case

--- projectileHandler.js
+++ projectileHandler.js
@@ -1,6 +1,6 @@
 class projectileHandler{
 
-  constructor(x,y,size,angle,type,damage){
+  constructor(x,y,size,angle,type,damage,target){
 
   this.type = type; //if type = 1 it is a magic attack if type = 2 it is a range attack 0 will be null
 	this.x = x;
@@ -8,6 +8,7 @@ class projectileHandler{
 	this.size = size;
 	this.angle = angle;
   this.damage = damage;
+  this.target = target; //not sure if i will need this so the projectile knows where to go
   //this.dir = "N"; direction
 
 }
@@ -16,14 +17,17 @@ setup(){
   switch (this.type){
 
     case 1: //mage
+    magicProjectile();
     //magic projectile data will go here
     break;
 
     case 2: //range
+    rangeProjectile();
     //range projectile data will go here
     break;
 
     case 3: //meele
+    meeleProjectile();
     //meele projectile data will go here
     break;
   }
@@ -37,4 +41,18 @@ setup(){
  update(){
 
  }
+
+ magicProjectile(){
+
+ }
+
+ rangeProjectile(){
+
+ }
+
+ meeleProjectile(){
+
+
+ }
+
 }
